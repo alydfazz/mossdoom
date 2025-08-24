f9ee308c48d9c3185f07f2d49c95e54239c4b147
alydfazz <alya.azzahra.difa@gmail.com>
2025-08-24 19:07:39 +0700

    first commit

--- script.js
+++ script.js
@@ -49,3 +49,9 @@ const navLinks = document.getElementById("nav-links");
 menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', () => {
+    navLinks.classList.remove('active');
+  });
+});
